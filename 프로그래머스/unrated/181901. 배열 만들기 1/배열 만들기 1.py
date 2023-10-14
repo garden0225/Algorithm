3b7f939f8232fa1389e72322dc590c3b905ab1df
garden0225 <98046714+garden0225@users.noreply.github.com>
2023-10-15 02:33:21 +0900

    [level unrated] Title: 배열 만들기 1, Time: 5.20 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181901. 배열 만들기 1/배열 만들기 1.py
+++ 프로그래머스/unrated/181901. 배열 만들기 1/배열 만들기 1.py
@@ -3,4 +3,8 @@ def solution(n, k):
     for i in range(1,n+1):
         if i%k==0:
             answer.append(i)
-    return sorted(answer)
+    return answer
+
+
+#def solution(n, k):
+#   return [i for i in range(k,n+1,k)]
