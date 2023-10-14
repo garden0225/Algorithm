a9ce1b727a851f13aeca68397f6d2709c50db008
garden0225 <98046714+garden0225@users.noreply.github.com>
2023-10-15 02:42:03 +0900

    [level unrated] Title: 카운트 다운, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181899. 카운트 다운/카운트 다운.py
+++ 프로그래머스/unrated/181899. 카운트 다운/카운트 다운.py
@@ -2,3 +2,5 @@ def solution(start, end_num):
     answer = [i for i in range(end_num,start+1)]
     
     return sorted(answer,reverse=True)
+
+#def solution(start, end):   return list(range(start,end-1,-1))
