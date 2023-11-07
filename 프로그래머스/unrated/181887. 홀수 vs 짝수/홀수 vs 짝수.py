16c0bbcb809ddf8d359848f450edc5f1a312be85
garden0225 <98046714+garden0225@users.noreply.github.com>
2023-11-07 18:50:26 +0900

    [level unrated] Title: 홀수 vs 짝수, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181887. 홀수 vs 짝수/홀수 vs 짝수.py
+++ 프로그래머스/unrated/181887. 홀수 vs 짝수/홀수 vs 짝수.py
@@ -1,5 +1,3 @@
 def solution(num_list):
-    if sum(num_list[::2]) > sum(num_list[1::2]):
-        return sum(num_list[::2])
-    else:
-        return sum(num_list[1::2])
+    return max(sum(num_list[::2]),sum(num_list[1::2]))
+     
