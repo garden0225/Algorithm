1ac7fc50d631b75081d547cd21a2f48f8780c1e2
garden0225 <98046714+garden0225@users.noreply.github.com>
2023-11-07 20:34:20 +0900

    [level unrated] Title: 왼쪽 오른쪽, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181890. 왼쪽 오른쪽/왼쪽 오른쪽.py
+++ 프로그래머스/unrated/181890. 왼쪽 오른쪽/왼쪽 오른쪽.py
@@ -1,8 +1,10 @@
 def solution(str_list):
+    if  "l" not in str_list and "r" not in str_list:    
+        return []
     for n, i in enumerate(str_list):
         if i =="l":
             return str_list[:n]
         elif i == "r":
-            return str_list[n+1:]  
-    return  []   
+            return str_list[n+1:] 
+   
             
