9238076a8e4b36abdef1ca4fd495a747aee7b4f0
garden0225 <98046714+garden0225@users.noreply.github.com>
2023-11-28 16:53:05 +0900

    [level unrated] Title: 문자열 계산하기, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/120902. 문자열 계산하기/문자열 계산하기.py
+++ 프로그래머스/unrated/120902. 문자열 계산하기/문자열 계산하기.py
@@ -7,3 +7,7 @@ def solution(my_string):
         elif op[i]=="-":
             answer-=int(op[i+1])
     return answer
+
+#def solution(my_string):
+#    return sum(int(i) for i in my_string.replace(' - ', ' + -').split(' + '))
+#미친사람,,, 5-8를 5 +(-8)로 변환
