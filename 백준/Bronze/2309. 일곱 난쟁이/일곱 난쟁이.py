02a8474de791f47bfff7d1f1e8c471c66669fa47
garden0225 <98046714+garden0225@users.noreply.github.com>
2024-03-19 23:28:15 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -1,10 +1,9 @@
-li = sorted([int(input()) for i in range(9)]) # 9명의 난쟁이 정렬
-
-for i in range(9): 
+li = sorted([int(input()) for i in range(9)])
+for i in range(9):
     for j in range(i+1,9):
-        if sum(li)-li[i]-li[j] == 100: # 모든 합 - 2개의 값 < 작으면 
-            remove1 = [li[i],li[j]] # 2개의 값들을 list 저장
-    
-for k in li:
-    if k not in remove1:
-        print(k)
+        if sum(li)-li[i]-li[j] == 100:
+            no_li = [li[i],li[j]] 
+            break
+for i in li:
+    if i not in no_li:
+        print(i)
