5eba86d07b3bf55caaa1434d0e53087cd86a8c41
garden0225 <98046714+garden0225@users.noreply.github.com>
2024-04-18 23:11:56 +0900

    [Silver IV] Title: 듣보잡, Time: 76 ms, Memory: 45096 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.py
+++ 백준/Silver/1764. 듣보잡/듣보잡.py
@@ -1,8 +1,6 @@
-n, m = map(int, input().split())
-arr = set([input() for _ in range(n)])
-name = []
-for i in range(m):
-    i = input()
-    if i in arr:
-        name.append(i)
-print(len(name),*sorted(name), sep='\n')
+import sys
+n,m=map(int,input().split())
+l=sys.stdin.readlines()
+u={*l[:n]}&{*l[n:]}
+print(len(u))
+print(''.join(sorted(u)))
